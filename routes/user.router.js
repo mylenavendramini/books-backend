3ba46e54be34c698fdd815092d73a2027df808d0
mylenavendramini <myvendramini@gmail.com>
2022-07-27 12:56:03 +0100

    user router

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const route = express.Router();
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middleware/auth");
 
-route.get("/", userController.getUsers);
+route.get("/", authMiddleware(["ADMIN"]), userController.getUsers);
+// route.get("/",  userController.getUsers);
 
 route.post("/", userController.postUsers);
+
+route.post("/login", userController.login);
 
 route.put("/:id", userController.putUsers);
 
