9c4c9e5af45e74f0c530694f28d18f77942bff7e
Mylena <myvendramini@gmail.com>
2023-03-08 18:20:42 +0000

    add cors

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8001;
-const db = require("./config/db");
+const db = require('./config/db');
+const cors = require('cors');
+app.use(cors());
 
-db.on("connected", () => {
-  console.log("connect");
+db.on('connected', () => {
+  console.log('connect');
 });
 
-db.on("disconnected", () => {
-  console.log("disconnected");
+db.on('disconnected', () => {
+  console.log('disconnected');
 });
 
-db.on("error", (error) => {
-  console.log("connection error:" + error);
+db.on('error', (error) => {
+  console.log('connection error:' + error);
 });
 
 app.use(express.json());
 
 // call function - import
-require("./config/routes")(app);
+require('./config/routes')(app);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on PORT ${PORT}!`);
