378ee7adf0153ac2472cf0f624a953f7e5a4bd06
Mylena <myvendramini@gmail.com>
2023-03-18 18:21:51 +0000

    change min-length

--- model/user.model.js
+++ model/user.model.js
@@ -1,48 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-let bcrypt = require("bcrypt");
+let bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   name: {
     type: String,
-    minlength: 5,
+    minlength: 2,
     maxlength: 20,
-    unique: [true, "This login already exists!"],
+    unique: [true, 'This login already exists!'],
   },
   email: {
     type: String,
     validate: {
       validator: function (email) {
         return new RegExp(
-          "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$"
+          '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$'
         ).test(email);
       },
     },
-    unique: [true, "This email already exists!"],
+    unique: [true, 'This email already exists!'],
   },
   password: {
     type: String,
     // select to false to don't show password when we get users. when we need the password, we need to use the select() method to change the select to true (e.g login in user.controller)
     select: false,
   },
-  admin: {
-    type: Boolean,
-  },
   permissions: {
     type: String,
-    enum: ["USER", "ADMIN"],
+    enum: ['USER', 'ADMIN'],
   },
-  books: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+  books: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
 });
 
 UserSchema.methods.generatePassword = function (password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
-UserSchema.pre("save", async function () {
+UserSchema.pre('save', async function () {
   this.password = await this.generatePassword(this.password);
 });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
